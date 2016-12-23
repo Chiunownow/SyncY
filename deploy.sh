da3b19861139ce55ececd40a2ac98670e963c83d
Chiunownow <hansey.z@gmail.com>
2016-12-23 11:56:34 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+mkdir /syncy /syncy/download/ /syncy/config/
+wget -P /syncy/config https://raw.githubusercontent.com/Chiunownow/SyncY/master/syncy
+wget -P /usr/bin/ https://raw.githubusercontent.com/Chiunownow/SyncY/master/syncyd.sh
+wget -P /usr/bin/ https://raw.githubusercontent.com/Chiunownow/SyncY/master/syncy.py
+chmod +x /usr/bin/syncy.py
+chmod +x /usr/bin/syncyd.sh
+rm install.sh
+syncy.py
